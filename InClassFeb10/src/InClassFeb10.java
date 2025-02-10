7650dc3a508823cd4f8b4c2599e3c5e0a01a7af8
Sanya  Raja <sanyaraja@Sanyas-MacBook-Air-2.local>
2025-02-10 12:04:35 -0700

    class work methods

---
+++ InClassFeb10/src/InClassFeb10.java
@@ -0,0 +1,16 @@
+
+public class InClassFeb10 {{
+int numGrad = 0;
+int numGrade = 0;
+if(numGrad >=60 && numGrade < 70)
+{
+	
+ String letterGrade = "D";
+}
+else if (numGrad >= 70 && numGrade < 80)
+{
+	String letterGrade = "C";
+
+}
+
+}}
---
+++ SandBox/src/MethodsAndDecisions.java
@@ -0,0 +1,43 @@
+
+public class MethodsAndDecisions {
+//  Access Modifier-keyword-return type - method name(Arguments)
+	public static void main(String[] args) {
+		int num1 = 2;
+		
+		int num2 = dblIt(num1);
+		System.out.println(num2);
+		
+		System.out.println(add (13,21));
+			
+
+	}
+
+	private static int dblIt(int n1) {
+		int doubled = n1 * 2;
+		return doubled;
+	}
+	
+	private static int add(int n1, int n2)
+	{
+		
+		return n1 + n2;
+	}}
+	
+	
+		
+		if (n1 > 10) {
+			return n1 * 2;
+		}
+		return n1 * n1;
+	}
+
+}
+   private static int  
+
+
+
+////!!!!! GROSS SHIT !!!!!
+///private static int dblIt(int n) {
+/// return n < 10 ? n * n:n *2;
+//}
+//}
